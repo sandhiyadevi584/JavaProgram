4c1273158e263215847faeb03558445e73a74634
Sandhiya Devi M <sandhiyadevi584@gmail.com>
2025-01-29 18:44:36 +0530

    Create OddOrEven.java

---
+++ OddOrEven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class OddEvenChecker {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = scanner.nextInt();
+    if (number % 2 == 0) {
+      System.out.println("The number is even.");
+      } else {
+      System.out.println("The number is odd.");
+      }
+    scanner.close();
+    }
+  }
