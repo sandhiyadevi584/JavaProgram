566a18189e3f99a65f9641f554a911f74b12942f
Sandhiya Devi M <sandhiyadevi584@gmail.com>
2025-02-01 19:56:36 +0530

    Create MultiplicationTable5.java

---
+++ MultiplicationTable5.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5;
+      System.out.println("Multiplication Table of " + number + " from 1 to 100:");
+      for (int i = 1; i <= 100; i++) {
+        System.out.println(number + " x " + i + " = " + (number * i));
+        }
+      }
+  }
