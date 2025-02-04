9f5c7a0b71e8e16da48e62ac318a094cf0f13038
Sandhiya Devi M <sandhiyadevi584@gmail.com>
2025-02-04 23:02:53 +0530

    Create VowelConsonant_Check.java
    1. The program takes a string input from the user.
    2. It then iterates through each character of the string.
    3. For each character, it checks whether it is a vowel or consonant. 4. If it is a vowel (a, e, i, o, u), it increments the vowel Count. 
    5. Otherwise, if it is a consonant, it increments the consonant Count.
    6. Finally, it prints the total number of vowels and consonants.
    This program ignores non-alphabet characters and handles both uppercase and lowercase letters.

---
+++ VowelConsonant_Check.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class VowelConsonantCheck {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String input = scanner.nextLine();
+    int vowelCount = 0;
+    int consonantCount = 0;
+    input = input.toLowerCase();
+    for (int i = 0; i < input.length(); i++) {
+      char ch = input.charAt(i);
+      if (Character.isLetter(ch)) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+          vowelCount++;
+          } else {
+          consonantCount++;
+          }
+        }
+      }
+    System.out.println("Number of vowels: " + vowelCount);
+    System.out.println("Number of consonants: " + consonantCount);
+    scanner.close();
+    }
+  }
